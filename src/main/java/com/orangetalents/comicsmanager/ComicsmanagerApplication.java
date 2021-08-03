066224aa4c03390cc8748f40920b58a6e131c955
braulio.carvalho@zup.com.br <braulio.carvalho@zup.com.br>
2021-08-03 13:45:04 -0300

    Creation of development branch, and addition of the spring project on github

---
+++ src/main/java/com/orangetalents/comicsmanager/ComicsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.orangetalents.comicsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangetalents/comicsmanager/ComicsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetalents.comicsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
